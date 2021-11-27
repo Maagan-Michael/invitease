7e2b3566089f3f1d1abdc682e0a3d560cf44972a
Inbar Barkai <inbarbarkai@gmail.com>
2021-11-27 10:04:45 +0200

    Renamed mainFunc to app.

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from sqlalchemy import create_engine #, engine
 from sqlalchemy import text
 from datetime import date
 
+app = FastAPI()
 
-mainFunc = FastAPI()
 templates = Jinja2Templates(directory='src/templates/')
 
 con = psycopg2.connect("dbname=invitease user=invitease host=db password=password")
@@ -17,7 +17,8 @@ cur = con.cursor()
 
 engine = create_engine("postgresql://invitease:password@db:5432/invitease")
 
-@mainFunc.get("/")
+# Samples:
+@app.get("/")
 def read_root():
    
     connection =  engine.connect()
@@ -26,17 +27,17 @@ def read_root():
     return {"Hello": a }
 
 
-@mainFunc.get("/items/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None, s: Optional[str] = None):
     return {"item_id": item_id, "q": q, "g": s}
 
 
-@mainFunc.get('/db/table/')
+@app.get('/db/table/')
 def form_post(request: Request):
     result = 'Choose a table'
     return templates.TemplateResponse('view_tables.html', context={'request': request, 'result': result})
 
-@mainFunc.post("/db/table/")
+@app.post("/db/table/")
 def form_post(request: Request,table_name: str = Form(...)):
     cur.execute(f"SELECT * FROM {table_name};")
 
@@ -47,23 +48,23 @@ def form_post(request: Request,table_name: str = Form(...)):
     # return {"{table} table": cur.fetchall()} 
 
 
-@mainFunc.get("/db/add/{table}/{column}/{value}")
+@app.get("/db/add/{table}/{column}/{value}")
 def read_item(table: str,column:str,value:str):
      cur.execute(f"INSERT INTO {table} ({column}) VALUES ('{value}');")
      return f'{value} added to {table}'
 
-@mainFunc.post("/db-test2/")
+@app.post("/db-test2/")
 async def example(request: Request):
     form_data = await request.form()
     return form_data
    
-@mainFunc.get('/db/add_guard')
+@app.get('/db/add_guard')
 def form_post(request: Request):
     result = 'Add a guard'
     return templates.TemplateResponse('add_guard.html', context={'request': request, 'result': result})
 
 
-@mainFunc.post('/db/add_guard')
+@app.post('/db/add_guard')
 def form_post(request: Request, guard_name: str = Form(...)):
 
     connection =  engine.connect()
@@ -76,13 +77,13 @@ def form_post(request: Request, guard_name: str = Form(...)):
     result = f'{guard_name} has been added as a guard'
     return templates.TemplateResponse('add_guard.html', context={'request': request, 'result': result, 'guard_name': guard_name})
 
-@mainFunc.get('/db/invite_guests')
+@app.get('/db/invite_guests')
 def form_post(request: Request):
     result = 'Invite a guest'
     return templates.TemplateResponse('invite_guests.html', context={'request': request, 'result': result})
 
 
-@mainFunc.post('/db/invite_guests')
+@app.post('/db/invite_guests')
 def form_post(request: Request, invitees_amount: str = Form(...)):
 
     currentDate = datetime.datetime.now()
@@ -95,4 +96,3 @@ def form_post(request: Request, invitees_amount: str = Form(...)):
     # con.commit()
     result = f'You have invited {invitees_amount} guests. Welcome!'
     return templates.TemplateResponse('add_guard.html', context={'request': request, 'result': result, 'invitees_amount': invitees_amount})
-
