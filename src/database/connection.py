cf31c4102ed5e34bc60500a8bc15df50bb8120f7
Inbar Barkai <inbarbarkai@gmail.com>
2021-11-28 08:29:48 +0200

    Fixed creating engine

--- src/database/connection.py
+++ src/database/connection.py
@@ -5,5 +5,5 @@ def create_connection():
     connection_string = getenv("IVT_CONNECTION_STRING","")
     if not connection_string:
         connection_string ="dbname=invitease user=invitease host=db password=password" 
-    engine = create_engine("postgresql://invitease:password@db:5432/invitease")
+    engine = create_engine(connection_string)
     return engine
