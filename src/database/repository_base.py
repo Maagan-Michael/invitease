45517d0a535c259728db24173da04abec0680c91
Inbar Barkai <inbarbarkai@gmail.com>
2021-12-02 21:05:24 +0200

    Implemented add/delete methods.

--- src/database/repository_base.py
+++ src/database/repository_base.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql import select
 from sqlalchemy.orm import Session
 
@@ -10,3 +11,18 @@ class RepositoryBase(object):
     def get_all(self):
         with Session(bind=self.connection) as session:
             return list(session.query(self.table))
+            
+    def add(self, entity):
+        with Session(bind=self.connection) as session:
+            session.add(entity)
+            session.commit()
+
+    def add_all(self, entities:list):
+        with Session(bind=self.connection) as session:
+            session.bulk_save_objects(entities)
+            session.commit()
+
+    def delete(self, entity):
+        with Session(bind=self.connection) as session:
+            session.delete(entity)
+            session.commit()
