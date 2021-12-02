13f7a2307e323842aa099835fb4f8246de4a83ea
Inbar Barkai <inbarbarkai@gmail.com>
2021-12-02 20:54:12 +0200

    Fixed datetime column definition.

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.types import DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
@@ -14,27 +15,27 @@ class User(Base):
     cellular_number = Column(String)
     email = Column(String(255))
     user_role = Column(String, nullable=False)
-    creation_timestamp = DateTime(timezone=True, nullable=False)
-    modify_timestamp = DateTime(timezone=True, nullable=False)
-
+    creation_timestamp = Column(DateTime(timezone=True), nullable=False)
+    modify_timestamp = Column(DateTime(timezone=True), nullable=False)
+    
 class Invitation(Base):
     __tablename__ = 'invitations'
 
     invitation_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True), nullable=False)
+    user_id = Column(UUID(as_uuid=True))
     invitees_amount = Column(Integer, nullable=False)
     invitees_admitted = Column(Integer, nullable=False)
     invitees_arrival_timestamp = DateTime(timezone=True)
     is_active = Column(Boolean, nullable=False)
-    creation_timestamp = DateTime(timezone=True, nullable=False)
-    modify_timestamp = DateTime(timezone=True, nullable=False)
+    creation_timestamp = Column(DateTime(timezone=True), nullable=False)
+    modify_timestamp = Column(DateTime(timezone=True), nullable=False)
     comment_for_guard = Column(String)
 
 class EventLogEntry(Base):
     __tablename__ = 'event_log'
 
     event_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    event_timestamp = DateTime(timezone=True, nullable=False)
+    event_timestamp = Column(DateTime(timezone=True), nullable=False)
     event_type = Column(String, nullable=False)
     amount_before = Column(Integer)
     amount_after = Column(Integer)
