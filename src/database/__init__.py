ee3d963052e26e86f980dd29fef00dc54a0b3135
Inbar Barkai <inbarbarkai@gmail.com>
2021-12-02 20:44:05 +0200

    Added EventLogEntry.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,3 @@
 from src.database.users_repository import UsersRepository
 from src.database.connection import create_connection
-from src.database.models import User, Invitation
+from src.database.models import User, Invitation, EventLogEntry
--- src/database/models.py
+++ src/database/models.py
@@ -8,24 +8,36 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
     cellular_number = Column(String)
-    email = Column(String)
-    user_role = Column(String)
-    creation_timestamp = DateTime(timezone=True)
-    modify_timestamp = DateTime(timezone=True)
+    email = Column(String(255))
+    user_role = Column(String, nullable=False)
+    creation_timestamp = DateTime(timezone=True, nullable=False)
+    modify_timestamp = DateTime(timezone=True, nullable=False)
 
 class Invitation(Base):
     __tablename__ = 'invitations'
 
-    invitation_id = Column(UUID(as_uuid=True), primary_key=True)
-    user_id = Column(UUID(as_uuid=True))
-    invitees_amount = Column(Integer)
-    invitees_admitted = Column(Integer)
+    invitation_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    invitees_amount = Column(Integer, nullable=False)
+    invitees_admitted = Column(Integer, nullable=False)
     invitees_arrival_timestamp = DateTime(timezone=True)
-    is_active = Column(Boolean)
-    creation_timestamp = DateTime(timezone=True)
-    modify_timestamp = DateTime(timezone=True)
+    is_active = Column(Boolean, nullable=False)
+    creation_timestamp = DateTime(timezone=True, nullable=False)
+    modify_timestamp = DateTime(timezone=True, nullable=False)
     comment_for_guard = Column(String)
+
+class EventLogEntry(Base):
+    __tablename__ = 'event_log'
+
+    event_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    event_timestamp = DateTime(timezone=True, nullable=False)
+    event_type = Column(String, nullable=False)
+    amount_before = Column(Integer)
+    amount_after = Column(Integer)
+    user_id = Column(UUID(as_uuid=True))
+    guard_id = Column(UUID(as_uuid=True))
+    invitation_id = Column(UUID(as_uuid=True))
