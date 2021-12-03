6089baaa75ef089d46927d48d195915be4cfa838
Inbar Barkai <inbarbarkai@gmail.com>
2021-12-03 14:48:03 +0200

    Implemented updating a user.

--- src/routers/admin_router.py
+++ src/routers/admin_router.py
@@ -1,13 +1,33 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime
+from fastapi import APIRouter, Depends, Request
 from database.connection import create_connection
 from database.users_repository import UsersRepository
+from pydantic import BaseModel
+from typing import List, Optional
 
 router = APIRouter(prefix="/admin", tags=["admin"])
+
 
 def create_users_repository():
     connection = create_connection()
     return UsersRepository(connection)
 
-@router.get("/users", tags=["users"])
+
+@router.get("/users")
 def read_users(users: UsersRepository = Depends(create_users_repository)):
-     return users.get_all()
+    return users.get_all()
+
+
+class UpdateUserRequest(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    cellular_number: Optional[str]
+    email: Optional[str]
+    is_active: Optional[bool]
+
+
+@router.post("/users/{user_id}")
+def update_user(user_id: str, request: UpdateUserRequest, users: UsersRepository = Depends(create_users_repository)):
+    update_data = request.dict(exclude_unset=True)
+    update_data ['modify_timestamp'] = datetime.utcnow()
+    users.update_item(user_id, update_data)
