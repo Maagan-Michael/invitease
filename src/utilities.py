6e5de7a17ddc356e46067e5afe8e41205ffa9d7b
Inbar Barkai <inbarbarkai@gmail.com>
2021-12-08 19:36:13 +0200

    Changed url to be dynamic.

--- src/utilities.py
+++ src/utilities.py
@@ -2,9 +2,25 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from database import UsersRepository, create_connection
 from fastapi import Request, Response
 import requests
+import os
 
-allowed_paths = ('/docs', '/openapi.json')
 
+class AuthenticationConfiguration:
+    def __init__(self) -> None:
+        self.openid_url = os.environ.get(
+            'OPENID_DISCOVERY_URL', 'http://localhost:8081/auth/realms/master/.well-known/openid-configuration')
+        self._get_user_info_endpoint()
+        self.allowed_paths = ('/docs', '/openapi.json')
+
+    def _get_user_info_endpoint(self):
+        response = requests.request(method='GET', url=self.openid_url)
+        if(response.status_code == 200):
+            global userinfo_endpoint
+            self.userinfo_endpoint = response.json()['userinfo_endpoint']
+        else:
+            raise ValueError("Failed to get user info endpoint.")
+
+configuration = AuthenticationConfiguration()
 
 def create_users_repository():
     connection = create_connection()
@@ -32,7 +48,7 @@ def openapi_override(app):
     openapi_schema['components']['securitySchemes'] = {
         "openId": {
             "type": "openIdConnect",
-            "openIdConnectUrl": "http://localhost:8081/auth/realms/master/.well-known/openid-configuration"
+            "openIdConnectUrl": configuration.openid_url
         }
     }
     openapi_schema['security'] = [{
@@ -48,10 +64,10 @@ def openapi_override(app):
 
 
 async def decode_token(request: Request, call_next):
-    if request.url.path.startswith(allowed_paths):
+    if request.url.path.startswith(configuration.allowed_paths):
         return await call_next(request)
     if request.state.token is not None:
-        user_response = requests.request(method='GET', url="http://localhost:8081/auth/realms/master/protocol/openid-connect/userinfo",
+        user_response = requests.request(method='GET', url=configuration.userinfo_endpoint,
                                          headers={"Authorization": f"Bearer {request.state.token}"})
         if user_response.status_code == 200:
             request.state.user = user_response.json()
@@ -63,11 +79,11 @@ async def decode_token(request: Request, call_next):
 
 
 async def extract_token(request: Request, call_next):
-    if request.url.path.startswith(allowed_paths):
+    if request.url.path.startswith(configuration.allowed_paths):
         return await call_next(request)
-    authotization_header = request.headers.get('Authorization')
-    if authotization_header is not None and authotization_header.startswith("Bearer"):
-        request.state.token = authotization_header[len("Bearer "):]
+    authorization_header = request.headers.get('Authorization')
+    if authorization_header is not None and authorization_header.startswith("Bearer"):
+        request.state.token = authorization_header[len("Bearer "):]
     else:
         request.state.token = None
     response = await call_next(request)
