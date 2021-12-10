a236ba0c40ecad48a85fa7e48db42f14a0238deb
yohai.barnea <yohai.bb@gmail.com>
2021-12-10 13:33:55 +0300

    get active invitations

--- src/routers/guard_router.py
+++ src/routers/guard_router.py
@@ -1,3 +1,13 @@
-from fastapi import APIRouter
+import fastapi as fa #fastapi comes with Docker. 
+import database as db
 
-router = APIRouter(prefix="/guard", tags=["guard"])
+router = fa.APIRouter(prefix="/guard", tags=["guard"])
+
+def create_invitations_list():
+    connection = db.create_connection()
+    return db.InvitationRepository(connection)
+
+@router.get("/activeinvitations", summary="Gets all the active invitations.")
+def get_activeinvitations(invitations: db.InvitationRepository = fa.Depends(create_invitations_list)):
+    return invitations.query(lambda x: x.filter(db.Invitation.is_active == True) 
+                            and x.sorted(db.Invitation.invitees_arrival_timestamp))
