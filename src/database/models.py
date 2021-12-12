7193f0b89e5eaeabb379023dd44d3f2027e6cc32
KamiTzayig <Dyekozz@gmail.com>
2021-12-12 23:30:06 +0200

    in order for the default values to be inserted when creating a new sql row you need to add default &server_default to the models.py in order for the sqlalchemy to expect the values to be inserted, also i created the restapi for creating a new order

--- src/database/models.py
+++ src/database/models.py
@@ -9,34 +9,34 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default='uuid_generate_v4()')
     first_name = Column(String(255), nullable=False)
     last_name = Column(String(255), nullable=False)
     cellular_number = Column(String)
     email = Column(String(255))
-    user_role = Column(String, nullable=False)
-    creation_timestamp = Column(DateTime(timezone=True), nullable=False)
-    modify_timestamp = Column(DateTime(timezone=True), nullable=False)
-    is_active = Column(Boolean, nullable=False)
+    user_role = Column(String, nullable=False, default='user')
+    creation_timestamp = Column(DateTime(timezone=True), nullable=False, server_default='NOW()')
+    modify_timestamp = Column(DateTime(timezone=True), nullable=False, server_default='NOW()')
+    is_active = Column(Boolean, nullable=False, default=True)
     
 class Invitation(Base):
     __tablename__ = 'invitations'
 
-    invitation_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True))
+    invitation_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default='uuid_generate_v4()')
+    user_id = Column(UUID(as_uuid=True), nullable=False)
     invitees_amount = Column(Integer, nullable=False)
-    invitees_admitted = Column(Integer, nullable=False)
-    invitees_arrival_timestamp = DateTime(timezone=True)
-    is_active = Column(Boolean, nullable=False)
-    creation_timestamp = Column(DateTime(timezone=True), nullable=False)
-    modify_timestamp = Column(DateTime(timezone=True), nullable=False)
+    invitees_admitted = Column(Integer, nullable=False, default=0)
+    invitees_arrival_timestamp = Column(DateTime(timezone=True), nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    creation_timestamp = Column(DateTime(timezone=True), nullable=False,server_default='NOW()')
+    modify_timestamp = Column(DateTime(timezone=True), nullable=False,server_default='NOW()')
     comment_for_guard = Column(String)
 
 class EventLogEntry(Base):
     __tablename__ = 'event_log'
 
-    event_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    event_timestamp = Column(DateTime(timezone=True), nullable=False)
+    event_id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default='uuid_generate_v4()')
+    event_timestamp = Column(DateTime(timezone=True), nullable=False, server_default='NOW()')
     event_type = Column(String, nullable=False)
     amount_before = Column(Integer)
     amount_after = Column(Integer)
--- src/routers/inviter_router.py
+++ src/routers/inviter_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from database import InvitationRepository, create_connection, models, Invitation
+from datetime import datetime
 
 router = APIRouter(prefix="/inviter", tags=["inviter"])
 
@@ -10,3 +11,15 @@ def create_invitations_list():
 @router.get("/invitations", summary="Gets all the invitations for this user.")
 def get_invitations(invitations: InvitationRepository = Depends(create_invitations_list), user_id: str = None):
     return invitations.query(lambda x: x.filter(Invitation.user_id == user_id))
+
+@router.get("/invite", summary="Creates a new invitation for this user.")
+def create_invitation(invitations: InvitationRepository = Depends(create_invitations_list), user_id: str = None, invitees_amount: int = None, invitees_arrival_timestamp_epoch: int = None,comment_for_guard: str =  None):
+    new_invitation =  Invitation(
+	user_id = user_id,
+	invitees_amount = invitees_amount,
+	invitees_arrival_timestamp = datetime.utcfromtimestamp(invitees_arrival_timestamp_epoch),
+	comment_for_guard = comment_for_guard,
+    )
+
+    invitations.add_item(new_invitation)
+    
