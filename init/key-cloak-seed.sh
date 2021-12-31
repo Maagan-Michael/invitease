4c0b75a6d4eb9efa16b85b5ea4ec5223189b7ba9
Yaron Shahrabani <sh.yaron@gmail.com>
2021-12-31 16:52:33 +0200

    Switched to original docker, modified seeding script and reorganized files (#59)
    * Switched to original docker, modified seeding script and reorganized files
    
    * Removed redundant comments and definitions

--- init/key-cloak-seed.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /opt/bitnami/keycloak/bin
-kcadm.sh config credentials --user admin --password admin --server http://localhost:8080/auth --realm master
-kcadm.sh create clients -s clientId=invitease -s 'redirectUris=["http://invitease.localhost/*"]' -s clientAuthenticatorType=client-secret -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000
-kcadm.sh update realms/master -s attributes.frontendUrl=http://keycloak.localhost/auth
--- init/windows.bat
+++
@@ -1,4 +0,0 @@
-SET DOCKER_COMPOSE=docker compose -f ..\docker-compose.yml
-%DOCKER_COMPOSE% cp .\key-cloak-seed.sh keycloak:/key-cloak-seed.sh 
-%DOCKER_COMPOSE% exec -u root keycloak sh /key-cloak-seed.sh 
-%DOCKER_COMPOSE% exec -u root keycloak rm /key-cloak-seed.sh 
