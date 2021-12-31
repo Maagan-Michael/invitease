aaf21967661b820712f2509396150a3c03eae156
Yaron Shahrabani <sh.yaron@gmail.com>
2021-12-31 11:15:30 +0200

    Switched to original docker, modified seeding script and reorganized files

--- init/linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DOCKER_COMPOSE="docker compose -f ../docker-compose.yml"
-eval "$DOCKER_COMPOSE cp ./key-cloak-seed.sh keycloak:/key-cloak-seed.sh"
-eval "$DOCKER_COMPOSE exec -u root keycloak sh /key-cloak-seed.sh"
-eval "$DOCKER_COMPOSE exec -u root keycloak rm /key-cloak-seed.sh"
