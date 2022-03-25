f5581a13f1f598e6f83f207f5ddd5f04245e7991
Inbar Barkai <inbarbarkai@gmail.com>
2022-03-25 11:57:46 +0300

    Fixed return type declaration.

--- src/database/users_repository.py
+++ src/database/users_repository.py
@@ -8,7 +8,7 @@ class UsersRepository(RepositoryBase):
     def __init__(self, connection) -> None:
         super().__init__(connection, User, User.user_id)
 
-    def get_user_by_id(self, user_ids: List[str] = []) -> list(User):
+    def get_user_by_id(self, user_ids: List[str] = []) -> list[User]:
         if len(user_ids) > 0:
             def filter(x):
                 return x.filter(User.user_id.in_(user_ids))
