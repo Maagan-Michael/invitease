ea0276acb351bf097aaeaf4f324b366a8518157b
Inbar Barkai <inbarbarkai@gmail.com>
2022-04-01 21:46:39 +0300

    Implemented getting all users.

--- src/keycloak/users_repository.py
+++ src/keycloak/users_repository.py
@@ -16,14 +16,17 @@ class KeycloakUserRepository(KeycloakClientBase):
     def __init__(self, access_token: str, base_url: str) -> None:
         super().__init__(access_token=access_token, base_url=base_url)
 
-    def get_user_by_id(self, user_ids: list[str] = []) -> list:
+    def get_all(self) -> list[User]:
+        return [json_to_user(x) for x in self._get_all_users()]
+
+    def get_user_by_id(self, user_ids: list[str] = []) -> list[User]:
         if len(user_ids) > 0:
             users = self._get_users_by_id(user_ids)
         else:
             users = self._get_all_users()
         return [json_to_user(x) for x in users]
 
-    def _get_users_by_id(self, user_ids: list[str] = []) -> list:
+    def _get_users_by_id(self, user_ids: list[str] = []) -> list[dict]:
         def get_user(id):
             url = self.build_url("users", id)
             headers = {"Authorization": f'Bearer {self.access_token}'}
@@ -33,11 +36,9 @@ class KeycloakUserRepository(KeycloakClientBase):
 
         return [get_user(x) for x in user_ids]
 
-    def _get_all_users(self):
+    def _get_all_users(self) -> list[dict]:
         url = self.build_url("users")
         headers = {"Authorization": f'Bearer {self.access_token}'}
         response = request('GET', url, headers=headers)
         response.raise_for_status()
         return response.json()
-
-
