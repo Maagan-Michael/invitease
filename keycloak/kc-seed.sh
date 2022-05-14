10d5636c95534788a9bb3bda7ed0f83f07d5793c
Yaron Shahrabani <yaron.shahrabani@cajarobotics.com>
2022-05-14 21:19:07 +0300

    Some docker fixes and improvements

--- keycloak/kc-seed.sh
+++ keycloak/kc-seed.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 (printf "%s" "waiting for Keycloak…"
-while ! (echo > /dev/tcp/localhost/9990) &> /dev/null
+while ! (echo > /dev/tcp/localhost/80) &> /dev/null
 do
     printf "%c" "."
     sleep 2
 done
-cd /opt/jboss/keycloak/bin
-./kcadm.sh config credentials --user admin --password admin --server http://localhost:8080/auth --realm master
+cd /opt/keycloak/bin
+./kcadm.sh config credentials --user admin --password admin --server http://localhost/ --realm master
 USER_ID=$(./kcadm.sh get users -r master --limit 1 --fields id | grep -oP '(?<="id" : ")[^"]*')
 ./kcadm.sh create clients \
     -s clientId=invitease \
-    -s 'redirectUris=["http://invitease.localhost/*"]' \
+    -s 'redirectUris=["http://app.localhost/*","http://dbadmin.localhost/*"]' \
     -s clientAuthenticatorType=client-secret -s serviceAccountsEnabled=true \
     -s directAccessGrantsEnabled=true \
     -s secret=d0b8122f-8dfb-46b7-b68a-f5cc4e25d000 
@@ -30,4 +30,4 @@ GROUP_ID=$(./kcadm.sh get groups -r master -q name=admin | grep -oP '(?<="id" : 
     -s 'config."multivalued"=true'
 ./kcadm.sh create groups -r master -s name=guards
 ./kcadm.sh create groups -r master -s name=users    
-./kcadm.sh update realms/master -s attributes.frontendUrl=http://keycloak.localhost/auth) &
+./kcadm.sh update realms/master -s attributes.frontendUrl=http://keycloak.localhost/) &
