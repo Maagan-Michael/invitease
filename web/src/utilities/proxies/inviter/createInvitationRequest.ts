ffd5de8e4b20a65f360489e8d90aa52d641279e6
Inbar Barkai <inbarbarkai@gmail.com>
2022-05-20 15:17:16 +0300

    Fixed casing.

--- web/src/utilities/proxies/inviter/createInvitationRequest.ts
+++ web/src/utilities/proxies/inviter/createInvitationRequest.ts
@@ -1,5 +1,5 @@
 export interface ICreateInvitationRequest {
-    invitees_amount: number;
-    is_active: boolean;
-    comment_for_guard: string;
+    inviteesAmount: number;
+    isActive: boolean;
+    commentForGuard: string;
 }
--- web/src/utilities/proxies/inviter/updateInvitationRequest.ts
+++ web/src/utilities/proxies/inviter/updateInvitationRequest.ts
@@ -1,5 +1,5 @@
 export interface IUpdateInvitationRequest{
-    invitees_amount: number;
-    invitees_arrival_timestamp_epoch: number;
-    comment_for_guard: string;
+    inviteesAmount: number;
+    inviteesArrivalTimestampEpoch: number;
+    commentForGuard: string;
 }
