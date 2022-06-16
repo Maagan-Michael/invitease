1341692436d97dd356b989be472a313f70f98135
Inbar Barkai <inbarbarkai@gmail.com>
2022-06-16 21:41:20 +0300

    Removed mock data.

--- web/src/features/inviter/inviterIntitations.tsx
+++ web/src/features/inviter/inviterIntitations.tsx
@@ -5,60 +5,13 @@ import { IInvitation } from '../../models/invitation';
 import { useApplicationContext } from '../../utilities/applicationContext';
 
 export function InviterInvitations() {
-    const u: IInvitation = {
-        invitationId: "1",
-        userId: "u1",
-        inviteesAmount: 4,
-        inviteesAdmitted: 3,
-        inviteesArrivalTimestamp: new Date(),
-        isActive: true,
-        creationTimestamp: new Date(),
-        modifyTimestamp: new Date(),
-        commentForGuard: "string",
-    };
-    const u2: IInvitation = {
-        invitationId: "2",
-        userId: "u1",
-        inviteesAmount: 2,
-        inviteesAdmitted: 2,
-        inviteesArrivalTimestamp: new Date(),
-        isActive: false,
-        creationTimestamp: new Date(),
-        modifyTimestamp: new Date(),
-        commentForGuard: "רכבים צהובים",
-    };
-
-    const u3: IInvitation = {
-        invitationId: "3",
-        userId: "u1",
-        inviteesAmount: 2,
-        inviteesAdmitted: 4,
-        inviteesArrivalTimestamp: new Date(),
-        isActive: false,
-        creationTimestamp: new Date(),
-        modifyTimestamp: new Date(),
-        commentForGuard: "",
-    };
-
-    const u4: IInvitation = {
-        invitationId: "4",
-        userId: "u1",
-        inviteesAmount: 5,
-        inviteesAdmitted: 5,
-        inviteesArrivalTimestamp: new Date(),
-        isActive: false,
-        creationTimestamp: new Date(),
-        modifyTimestamp: new Date(),
-        commentForGuard: "צבי כפרה עליך",
-    };
-    const [invitations, setInvitations] = React.useState([u, u2, u3, u4] as IInvitation[]);
+    const [invitations, setInvitations] = React.useState([] as IInvitation[]);
     const context = useApplicationContext();
     const inviterProxy = context.getInviterProxy();
     React.useEffect(() => {
         inviterProxy.getInvitations()
             .then(r => setInvitations(r));
     }, [inviterProxy]);
-
     return (
         <BasicTable invitations={invitations} />
     );
