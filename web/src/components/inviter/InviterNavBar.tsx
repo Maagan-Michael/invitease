7524a2be30e69cc19ecd891a1a552df5824e3335
Inbar Barkai <inbarbarkai@gmail.com>
2022-07-15 16:24:29 +0300

    Extracted logo to an external component.

--- web/src/components/inviter/InviterNavBar.tsx
+++ web/src/components/inviter/InviterNavBar.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import "../../index.css"
-import logo from "../../images/mm-logo.png"
+import Logo from "../logo";
 
 export function InviterNavBar() {
     return (
         <header className="header">
             <nav className="nav">
                 <div className="nav">
-                    <img src={logo} className="App-logo-small" alt="logo" />
-                    <h1 className="nav_title">Invitease</h1>
+                    <Logo />
                 </div>
                 <ul className="nav_items">
                     <li>הזמנות שלי</li>
